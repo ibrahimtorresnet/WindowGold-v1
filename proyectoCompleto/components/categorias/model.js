e4ffd1bd003c6df0f1442eb91b27a503cebe6882
ibrahimtorresnet <146012872+ibrahimtorresnet@users.noreply.github.com>
2024-03-09 12:43:31 -0400

    Delete proyectoCompleto directory

--- proyectoCompleto/components/categorias/model.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const database = require('../../db/index.js');
-const {
-  getFirestore
-} = require('firebase-admin/firestore');
-
-const db = getFirestore(admin.apps[database]);
-
-module.exports = db;
